fafa5e428429437b02ece299f4fa53aaaf75bc05
imyroslav <imyroslav@ukr.net>
2025-06-07 21:03:04 +0200

    update-pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
 import toast, { Toaster } from "react-hot-toast";
 import css from "./App.module.css"
 import SearchBar from "../SearchBar/SearchBar";
@@ -17,16 +18,21 @@ export default function App() {
 
     const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null)
     const [currentQuery, setCurrentQuery] = useState("");
+    const [currentPage, setCurrentPage] = useState(1);
 
-    const { data, isLoading, isError } = useQuery<GetMovies>({
-        queryKey: ["movies", currentQuery],
-        queryFn: () => fetchMovies(currentQuery),
+    const { data, isLoading, isError , isSuccess} = useQuery<GetMovies>({
+        queryKey: ["movies", currentQuery, currentPage],
+        queryFn: () => fetchMovies(currentQuery, currentPage),
         enabled: currentQuery !== "",
+        placeholderData: keepPreviousData,
     })
 
     const handleSearch = (newQuery: string) => {
-        setCurrentQuery(newQuery)
+        setCurrentQuery(newQuery);
+        setCurrentPage(1);
     };
+
+    
 
     const openModal = (movie: Movie) => {
         setSelectedMovie(movie);
@@ -36,11 +42,26 @@ export default function App() {
         setSelectedMovie(null);
     };
 
+    const totalPages = data?.total_pages ?? 0
+
     return (
         <div className={css.app}>
             <SearchBar onSubmit={handleSearch} />
+            {isSuccess && totalPages > 1 && (
+                <ReactPaginate
+                pageCount={totalPages}
+                pageRangeDisplayed={5}
+                marginPagesDisplayed={1}
+                onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+                forcePage={currentPage - 1}
+                containerClassName={css.pagination}
+                activeClassName={css.active}
+                nextLabel="→"
+                previousLabel="←"
+                />
+            )}
+            {isSuccess && data?.results.length === 0 && toast.error("No movies found on your request")}
             <Toaster />  
-            {data?.results && data.results.length === 0 && toast.error("No movies found on your request")}
             {isLoading && <Loader />}
             {isError && <ErrorMessage />}
             {data?.results && data.results.length > 0 && <MovieGrid onSelect={openModal} movies={data.results} />}
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -4,10 +4,14 @@ import { type Movie } from "../types/movie";
 
 export interface GetMovies {
     results: Movie[];
+    page: number;
+    total_pages: number;
 }
 
 
-export const fetchMovies = async(newQuery: string): Promise<GetMovies> => {
+export const fetchMovies = async (
+    newQuery: string,
+    page: number = 1): Promise<GetMovies> => {
     
     const reqConfig = {
         url: "https://api.themoviedb.org/3/search/movie",
@@ -18,7 +22,7 @@ export const fetchMovies = async(newQuery: string): Promise<GetMovies> => {
     }
     
     const reply = await axios.get<GetMovies>(
-        `${reqConfig.url}?query=${newQuery}`,
+        `${reqConfig.url}?query=${newQuery}&page=${page}`,
         reqConfig
     );
     return reply.data;
